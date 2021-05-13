18f41963537a42717407752c6f0246981d0b8038
Neumar Malheiros <neumar@nacbook.local>
2021-05-13 14:18:57 -0300

    iniciando a app

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express')
+
+var app = express()
+
+app.set('port', 5000)
+
+
+app.get('/hello', function(req, res){
+    res.end('WS esta funcionando!')
+})
+
+app.get('/dobro/:n', function(req, res){
+    d = req.params.n * 2
+    res.end('resposta: '+d)
+})
+
+app.listen(app.get('port'), function(){
+    console.log('app running on port ', app.get('port'))
+})
